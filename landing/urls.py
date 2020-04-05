c5561e9dd238b0d2ae8540234bf2e1d15724be4a
Tchelex <tchelex@mail.ru>
2020-04-05 18:03:46 +0300

    Initial commit

---
+++ landing/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.views.generic.base import TemplateView
+
+
+urlpatterns = [
+    path('', TemplateView.as_view(
+        template_name="landing/index.html"
+    ), name='home'),
+    path('base/', TemplateView.as_view(
+        template_name="landing/base.html"
+    ), name='base')
+]
